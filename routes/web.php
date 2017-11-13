bcdbca907c39e2877d5f3396d907579f3bb7d638
Faisal Abdul Hamid <faisal.abdulhamid@gmail.com>
2017-11-13 15:18:09 +0700

    add-middleware

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,22 @@
 <?php
 use Illuminate\Support\Facades\Auth;
 
-Route::get('/', function () {
-    return view('auth.login');
-});
-
 Route::get('/user', function(){
   $user = Auth::user();
   return response()->json($user, 200);
-});//->middleware('auth');
+})->middleware('auth');
 
 
-Route::resource('proyek', 'ProyekController');
-Route::resource('resiko', 'ResikoController');
-Route::resource('pekerjaan', 'PekerjaanController');
-Route::resource('bahan-baku', 'BahanBakuController');
-Route::resource('pegawai', 'PegawaiController');
+Route::resource('proyek', 'ProyekController')->middleware('auth');
+Route::resource('resiko', 'ResikoController')->middleware('auth');
+Route::resource('pekerjaan', 'PekerjaanController')->middleware('auth');
+Route::resource('bahan-baku', 'BahanBakuController')->middleware('auth');
+Route::resource('pegawai', 'PegawaiController')->middleware('auth');
 
-Route::prefix('detail')->name('detail')->group(function(){
+Route::prefix('detail')->name('detail')->middleware('auth')->group(function(){
     Route::get('/{param}', 'DetailProyekController@index')->name('.index');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home')->middleware('auth');
